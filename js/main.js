02661cf8bb9354397e49331504f9df3cda6a4ce3
karolinamilka <lapko.karolina@mail.ru>
2020-11-01 20:14:31 +0300

    Removed one Line

---
+++ js/main.js
@@ -0,0 +1,110 @@
+window.onload = function () {
+    var header1 = document.getElementById("tabHeader_1");
+    var header2 = document.getElementById("tabHeader_2");
+    header1.onclick = onHeader1Click;
+    header2.onclick = onHeader2Click;
+    function onHeader1Click() {
+        document.getElementById("tabpage_2").style.display = "none";
+        document.getElementById("tabpage_1").style.display = "flex";
+        header1.setAttribute("class", "tabActiveHeader");
+        document.getElementById("tabHeader_2").removeAttribute("class");
+
+    }
+    function onHeader2Click() {
+        document.getElementById("tabpage_2").style.display = "flex";
+        document.getElementById("tabpage_1").style.display = "none";
+        header2.setAttribute("class", "tabActiveHeader");
+        document.getElementById("tabHeader_1").removeAttribute("class");
+    }
+
+    var sliderData = [
+        {
+            src: '../img/logo.png',
+            type: 'web'
+        },
+        {
+            src: '../img/logo.png',
+            type: 'photo'
+        },
+        {
+            src: '../img/logo.png',
+            type: 'design'
+        }
+    ]
+
+    var sliderPhoto = document.getElementById("slider-photo");
+
+    
+    
+}
+
+var readMoreData = [
+    'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, sint tempore. Facilis aliquid animi debitis accusantium, voluptas ut culpa quis.',
+    'Lorem lorem lorem',
+    'ipsum ipsum ipsum',
+    'dolor dolor dolor'
+]
+
+function openModal(i) {
+    document.getElementById("modal").style.display = "block";
+    console.log(i);
+    document.getElementById("modal-content").innerText = readMoreData[i]
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+function OnButtonClick (type) {
+    var sliderData = [
+        {
+            src: ['./img/slider.jpg', './img/slider.jpg' , './img/slider.jpg' , './img/slider.jpg' , './img/slider.jpg'],
+            type: 'web',
+            text: 'vfhfvhjfhgfh'
+        },
+        {
+            src: ['./img/slider.jpg', './img/slider.jpg' , './img/slider.jpg' , './img/slider.jpg' , './img/slider.jpg'],
+            type: 'photo',
+            text: 'vfhfvhjfhgfh'
+        },
+        {
+            src: ['./img/slider.jpg', './img/slider.jpg' , './img/slider.jpg' , './img/slider.jpg' , './img/slider.jpg'],
+            type: 'design',
+            text: 'vfhfvhjfhgfh'
+        }
+    ]
+    var sliderPhoto = document.getElementById("slider-photo");
+    // document.getElementById("slider-photo") = '<div class="slider-photo"></div>'
+    for (i=0; i<sliderData.length; i++) {
+        if (sliderData[i].type === type) {
+            for (x=0; x<sliderData[i].src.length; x++) {
+                addAttributes()
+            }
+        }
+    }
+    if (type === 'all') {
+    for (i=0; i<sliderData.length; i++) {
+        
+            for (x=0; x<sliderData[i].src.length; x++) {
+                addAttributes() 
+            }
+        
+    }
+}
+
+function addAttributes(){
+    var divPhoto = document.createElement('div')
+                divPhoto.setAttribute('class', 'slider-photo-div')
+                var text = document.createElement('div')
+                text.setAttribute('class', 'slider-text')
+                text.appendChild(document.createTextNode(sliderData[i].text))
+                var photo = document.createElement('img')
+                photo.setAttribute('src', sliderData[i].src[x])
+                photo.setAttribute('title', 'slider-photo-div')
+                photo.setAttribute('class', 'slider-photo')
+                divPhoto.appendChild(photo)
+                divPhoto.appendChild(text)
+                sliderPhoto.appendChild(divPhoto)
+}
+
+}
